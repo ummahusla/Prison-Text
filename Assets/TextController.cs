28ab15c856b1714f4f4ad44fb62eda714bb31e92
Edvins Antonovs <ummahusla@gmail.com>
2016-11-14 18:56:34 +0000

    Added enum States and myStates variables

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -5,19 +5,38 @@ using System.Collections;
 public class TextController : MonoBehaviour {
 
 	public Text text;
-
+	
+	private enum States {cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, freedom};
+	private States myState;
+	
 	// Use this for initialization
 	void Start () {
-		text.text = "Press Space to start the game!";
+		myState = States.cell;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space)) {
-			text.text = "You are in a prison cell and you want to escape. There are " + 
-						"some dirty sheets on the bed, a mirror on the wall, and the door " + 
-						"is locked from the outside.\n\n" + 
-						"Press S to view Sheets, M to view Mirror and L to view Lock";
+		print(myState);
+	}
+	
+	void state_cell () {
+	
+		text.text = "You are in a prison cell and you want to escape. There are " + 
+					"some dirty sheets on the bed, a mirror on the wall, and the door " + 
+					"is locked from the outside.\n\n" + 
+					"Press S to view Sheets, M to view Mirror and L to view Lock";
+					
+		if(Input.GetKeyDown(KeyCode.S)) {
+			
 		}
+		
+		if(Input.GetKeyDown(KeyCode.M)) {
+			
+		}
+		
+		if(Input.GetKeyDown(KeyCode.L)) {
+			
+		}
+		
 	}
 }
