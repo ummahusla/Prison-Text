d91918f5afe64cf11b590d747fd6159dfdc13598
Edvins Antonovs <ummahusla@gmail.com>
2016-11-14 20:02:15 +0000

    Added state_sheet_0 method.

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -18,6 +18,8 @@ public class TextController : MonoBehaviour {
 	void Update () {
 		if(myState == States.cell) {
 			state_cell();
+		} else if(myState == States.sheets_0) {
+			state_sheet_0();
 		}
 	}
 	
@@ -26,19 +28,25 @@ public class TextController : MonoBehaviour {
 		text.text = "You are in a prison cell and you want to escape. There are " + 
 					"some dirty sheets on the bed, a mirror on the wall, and the door " + 
 					"is locked from the outside.\n\n" + 
-					"Press S to view Sheets, M to view Mirror and L to view Lock";
+					"Press S to view Sheets, M to view Mirror and L to view Lock.";
 					
 		if(Input.GetKeyDown(KeyCode.S)) {
 			myState = States.sheets_0;
-		}
-		
-		if(Input.GetKeyDown(KeyCode.M)) {
-			
-		}
-		
-		if(Input.GetKeyDown(KeyCode.L)) {
-			
-		}
+		}	
 		
 	}
+	
+	void state_sheet_0 () {
+		
+		text.text = "You can't believe you sleep in this things. Surely it's " + 
+					"time somebody change them. The pleasure of prison life" + 
+					"I guess!\n\n" + 
+					"Press R to return to roaming your cell.";
+		
+		if(Input.GetKeyDown(KeyCode.R)) {
+			myState = States.cell;
+		}	
+		
+	}
+	
 }
