3a26fe7e86f2e931fb1a779b506810771e36eef8
Edvins Antonovs <ummahusla@gmail.com>
2016-11-14 18:39:49 +0000

    Updated the game intro with description.

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -8,11 +8,16 @@ public class TextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		text.text = "Press Space to start the game!";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			text.text = "You are in a prison cell and you want to escape. There are " + 
+						"some dirty sheets on the bed, a mirror on the wall, and the door " + 
+						"is locked from the outside.\n\n" + 
+						"Press S to view Sheets, M to view Mirror and L to view Lock";
+		}
 	}
 }
