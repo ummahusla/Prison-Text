7059e85a351f74b2f5e11627361edfd4da17600b
Edvins Antonovs <ummahusla@gmail.com>
2016-11-14 21:09:32 +0000

    Completed implementation of the game loop

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -10,22 +10,33 @@ public class TextController : MonoBehaviour {
 	private States myState;
 	
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		myState = States.cell;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		if(myState == States.cell) {
 			state_cell();
 		} else if(myState == States.sheets_0) {
 			state_sheet_0();
+		} else if(myState == States.sheets_1) {
+			state_sheet_1();
 		} else if(myState == States.lock_0) {
 			state_lock_0();
+		} else if(myState == States.lock_1) {
+			state_lock_1();
+		} else if(myState == States.mirror) {
+			state_mirror();
+		} else if (myState == States.cell_mirror) {
+			state_cell_mirror();
+		} else if (myState == States.freedom) {
+			state_freedom();
 		}
+		
 	}
 	
-	void state_cell () {
+	void state_cell() {
 	
 		text.text = "You are in a prison cell and you want to escape. There are " + 
 					"some dirty sheets on the bed, a mirror on the wall, and the door " + 
@@ -36,11 +47,13 @@ public class TextController : MonoBehaviour {
 			myState = States.sheets_0;
 		} else if(Input.GetKeyDown(KeyCode.L)) {
 			myState = States.lock_0;
+		} else if(Input.GetKeyDown(KeyCode.M)) {
+			myState = States.mirror;
 		}	
 		
 	}
 	
-	void state_sheet_0 () {
+	void state_sheet_0() {
 		
 		text.text = "You can't believe you sleep in this things. Surely it's " + 
 					"time somebody change them. The pleasure of prison life" + 
@@ -53,7 +66,7 @@ public class TextController : MonoBehaviour {
 		
 	}
 	
-	void state_sheet_1 () {
+	void state_sheet_1() {
 	
 		text.text = "Holding a mirror in your hand doesn't make the sheets look " +
 					"any better.\n\n" +
@@ -65,7 +78,7 @@ public class TextController : MonoBehaviour {
 	}
 	
 	
-	void state_lock_0 () {
+	void state_lock_0() {
 		
 		text.text = "This is one of the button locks. You have no idea what the " + 
 					"combination is. You wish you could somehow see where the dirty " + 
@@ -78,7 +91,7 @@ public class TextController : MonoBehaviour {
 		
 	}
 	
-	void state_lock_1 () { 
+	void state_lock_1() { 
 	
 		text.text = "You carefully put the mirror through the bars, and turn it round " +
 					"so you can see the lock. You can just make out fingerprints around " +
@@ -86,9 +99,46 @@ public class TextController : MonoBehaviour {
 					"Press O to Open, or R to Return to your cell.";
 					
 		if(Input.GetKeyDown(KeyCode.O)) {
-			myState = States.corridor_0;
+			myState = States.freedom;
 		} else if(Input.GetKeyDown(KeyCode.R)) {
 			myState = States.cell_mirror;
+		}
+	}
+	
+	void state_mirror() {
+	
+		text.text = "The dirty old mirror on the wall seems loose.\n\n" +
+					"Press T to Take the mirror, or R to Return to cell" ;
+			
+		if(Input.GetKeyDown(KeyCode.T)) {
+			myState = States.cell_mirror;
+		} else if (Input.GetKeyDown(KeyCode.R)) {
+			myState = States.cell;
+		}
+		
+	}
+	
+	void state_cell_mirror() {
+	
+		text.text = "You are still in your cell, and you STILL want to escape! There are " +
+					"some dirty sheets on the bed, a mark where the mirror was, " +
+					"and that pesky door is still there, and firmly locked!\n\n" +
+					"Press S to view Sheets, or L to view Lock" ;
+					
+		if (Input.GetKeyDown(KeyCode.S)) {
+			myState = States.sheets_1;
+		} else if (Input.GetKeyDown(KeyCode.L)) {
+			myState = States.lock_1;
+		}
+		
+	}
+	
+	void state_freedom() {
+		text.text = "You are FREE!\n\n" + 
+					"Press P to play again.";
+					
+		if (Input.GetKeyDown(KeyCode.P)) {
+			myState = States.cell;
 		}
 	}
 	
